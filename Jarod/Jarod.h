963d89f567b4b82be8015c57d7f8deff7190f762
Jānis Kiršteins <janis@tawk.to>
2015-01-20 16:42:06 +0200

    changes

---
+++ Jarod/Jarod.h
@@ -0,0 +1,19 @@
+//
+//  Jarod.h
+//  Jarod
+//
+//  Created by Jānis Kiršteins on 20/01/15.
+//  Copyright (c) 2015 Jānis Kiršteins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jarod.
+FOUNDATION_EXPORT double JarodVersionNumber;
+
+//! Project version string for Jarod.
+FOUNDATION_EXPORT const unsigned char JarodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jarod/PublicHeader.h>
+
+
